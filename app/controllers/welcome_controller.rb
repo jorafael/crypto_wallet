f6144b861f4142c73136e5a3dec87fe15c1cf3a5
jorafael <jorafael@gmail.com>
2019-07-25 17:13:18 -0300

    [Add] Incluído mudança de idioma em tempo de execução

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    cookies[:curso] = "Curso de ruby on rails 5.x [COOKIES]"
+    session[:curso] = "Curso de ruby on rails 5.x [SESSION]"
     @nome = params[:nome]
     @curso = params[:curso]
     
