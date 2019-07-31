4f7a3b714c1cc4481849e67ac9e78ce4c8f87e49
jorafael <jorafael@gmail.com>
2019-07-31 10:36:56 -0300

    Usando o Asset Pipeline

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -1,5 +1,5 @@
 class CoinsController < ApplicationController
-  layout 'adm'
+  # layout 'adm'
 
   before_action :set_coin, only: [:show, :edit, :update, :destroy]
   before_action :set_mining_type_options, only: [:new, :create, :edit, :update]
@@ -45,7 +45,7 @@ class CoinsController < ApplicationController
   def update
     respond_to do |format|
       if @coin.update(coin_params)
-        format.html { redirect_to @coin, notice: 'Coin was successfully updated.'}
+        format.html { redirect_to @coin, notice: 'Coin was successfully updated.' }
         format.json { render :show, status: :ok, location: @coin }
       else
         format.html { render :edit }
@@ -66,17 +66,17 @@ class CoinsController < ApplicationController
 
   private
 
-    def set_mining_type_options
-      @mining_type_options = MiningType.all.pluck(:description, :id)
-    end
-    
-    # Use callbacks to share common setup or constraints between actions.
-    def set_coin
-      @coin = Coin.find(params[:id])
-    end
+  def set_mining_type_options
+    @mining_type_options = MiningType.all.pluck(:description, :id)
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def coin_params
-      params.require(:coin).permit(:description, :acronym, :url_image, :mining_type_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_coin
+    @coin = Coin.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def coin_params
+    params.require(:coin).permit(:description, :acronym, :url_image, :mining_type_id)
+  end
 end
